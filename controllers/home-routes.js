a91915bd1ad095e83b65c1d5a5eeece71d18a51a
Criste Phillips <criste.phillips@yahoo.com>
2022-03-25 15:49:15 -0400

    findbyPk fxn repaired

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 // home-routes.js contains all user-facing routes (homepage & login)
 const router = require('express').Router();
-// const sequelize = require("../config/connection");
+const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
